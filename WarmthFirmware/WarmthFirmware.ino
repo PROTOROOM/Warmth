83d3956fb560241660c69e925fcdc1319a9c9efc
SeungBum Kim <picxenk@gmail.com>
2017-03-19 19:29:34 +0900

    update

--- WarmthFirmware/WarmthFirmware.ino
+++ WarmthFirmware/WarmthFirmware.ino
@@ -1,10 +1,10 @@
 /*
 	# Warmth : light device for warmth
-        - https://github.com/PROTOROOM/Warmth
-	- by SeungBum Kim & Hoonida Kim
-	- Artistic License 2.0  http://choosealicense.com/licenses/artistic-2.0/
-	
-*/
+ - https://github.com/PROTOROOM/Warmth
+ 	- by SeungBum Kim & Hoonida Kim
+ 	- Artistic License 2.0  http://choosealicense.com/licenses/artistic-2.0/
+ 	
+ */
 
 int analogInput = A1;
 int analogOutput = 1;
@@ -21,57 +21,67 @@ long pTime = 0;
 long cTime = 0;
 int fade = 20;
 int brightness = 0;
+int pulseCount = 0;
+int minValue = 30;
+int defaultValue = 130;
 
 void setup() {                
   pinMode(analogOutput, OUTPUT);
 
-  total = 0;
-  for (int i=0; i<numReadings; i++) {
-    total = total + analogRead(analogInput);
-    if (numReadings%2 == 0) writeValue(100);
-    else writeValue(0);
-    delay(100);
-  }
-  average = total / numReadings;
+  averageTemp(1);
+  brightness = defaultValue;
 }
 
 
 // ##########################  MAIN  ##########################
 void loop() {
-	
-//  nTemp = analogRead(analogInput);
-//  minTemp = min(minTemp, nTemp);
-//  maxTemp = max(maxTemp, nTemp);
 
-  current = analogRead(analogInput);
+//  current = analogRead(analogInput);
+  current = readSmooth();
   cTime = millis();
   
-  if (!triggered && abs(average - current) > 10) {
-    triggered = true;
-  }
-  
+//  beatPulse(60);
+
   if (triggered) {
-//    beatPulse(70);
+//    beatPulse(60);
     for (int i=0; i<5; i++) {
-      writeValue(250);
-      delay(300);
-      writeValue(50);
-      delay(300);
+      beatPulse2(40);
+    }
+    
+    for (;brightness < defaultValue; brightness+=2) {
+      writeValue(brightness);
+      delay(20);
     }
     triggered = false;
+
+//    if (pulseCount > 7) {
+//      triggered = false;
+//    }
+
   } else {
-    writeValue(130);
+
+    
+    writeValue(brightness);
+//    if (brightness > 150) brightness--;
+    
+    if ((current - average) > 10) {
+      triggered = true;
+      pulseCount = 0;
+    }
+
   }
-//  slowOn(0.01);
+
+
 }
 
 
 void beatPulse(int beat) {  
-  if (brightness == 0) {
+  if (brightness <= 0) {
     fade = 20;
   }
-  if (brightness == 240) {
+  if (brightness >= 255) {
     fade = -5;
+    pulseCount++;
   }
 
   if (cTime - pTime > int(1000/beat)) {
@@ -81,61 +91,100 @@ void beatPulse(int beat) {
   }
 }
 
+void beatPulse2(int beat) {
+//  brightness = 0;
+  for (;brightness<255;brightness+=15) {
+    writeValue(brightness);
+    delay(1000/beat);
+  }
+  for (;brightness>minValue; brightness-=5) {
+    writeValue(brightness);
+    delay(1000/beat);
+  }
+}
 
+
+void averageTemp(boolean withLight) {
+  int t = 0;
+  int n = 30;
+  for (int i=0; i<n; i++) {
+    t = t + analogRead(analogInput);
+
+    if (withLight) {
+      if (n%2 == 0) writeValue(100);
+      else writeValue(0);
+    }
+
+    delay(100);
+  }
+  average = t / n;
+}
+
+int readSmooth() {
+  total = total - readings[readIndex];
+  readings[readIndex] = analogRead(analogInput);
+  total = total + readings[readIndex];
+  readIndex = readIndex + 1;
+  
+  if (readIndex >= numReadings) readIndex = 0;
+  return total / numReadings;
+}
 
 
 // Analog Input
 int readValue() {
-	return map(analogRead(analogInput), 0, 1023, 0, 255);
+  return map(analogRead(analogInput), 0, 1023, 0, 255);
 }
 
 
 float readTemp() {
   int reading = analogRead(analogInput);
   float voltage = reading * 3.0/1024.0;
-//  float cTemp = (voltage - 0.5) * 100;
-//  return cTemp;
+  //  float cTemp = (voltage - 0.5) * 100;
+  //  return cTemp;
   return voltage;
-//  return map(cTemp, 20, 32, 0, 255);
+  //  return map(cTemp, 20, 32, 0, 255);
 }
 
 int calibrateLightSensor(int light) {
-    int mid = 600;
-    int mid2 = 900;
-    if ( light < mid) {
-        light = int(round((40.0/mid)*light));
-    } else if (light < mid2) {
-        light = int(round((mid2-40)/(mid2-float(mid))* light) - 1680);
-    }
-    light = constrain(light, 0, 1023);    
-    return light;
+  int mid = 600;
+  int mid2 = 900;
+  if ( light < mid) {
+    light = int(round((40.0/mid)*light));
+  } 
+  else if (light < mid2) {
+    light = int(round((mid2-40)/(mid2-float(mid))* light) - 1680);
+  }
+  light = constrain(light, 0, 1023);    
+  return light;
 }
 
 
 // Analog Output
 void writeValue(int value) {
-	analogWrite(analogOutput, value);
+  analogWrite(analogOutput, value);
 }
 
 void slowOn(float waitSec) {
-	for (int i=0; i<=255; i=i+5) {
-		writeValue(i);
-		wait(waitSec);
-	}
+  for (int i=0; i<=255; i=i+5) {
+    writeValue(i);
+    wait(waitSec);
+  }
 }
 
 void slowOff(float waitSec) {
-	for (int i=255; i>=0; i=i-5) {
-		writeValue(i);
-		wait(waitSec);
-	}	
+  for (int i=255; i>=0; i=i-5) {
+    writeValue(i);
+    wait(waitSec);
+  }	
 }
 
 
 
 // Common
 void wait(float sec) {
-	delay(sec*1000);
+  delay(sec*1000);
 }
+
 
 
